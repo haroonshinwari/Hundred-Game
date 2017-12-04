976e7a5182682e5cf17e616445aec52e35a4c1b5
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2017-12-04 17:22:47 +0000

    added description along with each function

--- hundred.py
+++ hundred.py
@@ -3,19 +3,19 @@
 
 fill in description of the game'''
 
-def main():
+def main():                                         #this is the fuction that will start the programme for execution
 
-def rules():
+def rules():                                        #function for printing the rules and instructions for the player
 
-def human_move(computer_score, human_score):
+def human_move(computer_score, human_score):        #function to run when it is the human turn/move
 
-def computer_move(computer_score, human_score):
+def computer_move(computer_score, human_score):     #function to run when it is the computer's turn/move
 
-def is_game_over(computer_score, human_score):
+def is_game_over(computer_score, human_score):      #function to determine whether there is a winner or not and if the game is over
 
-def roll():
+def roll():                                         #function to roll the die randomly        
 
-def ask_yes_or_no(prompt):
+def ask_yes_or_no(prompt):                          #function to ask if the user would like to roll again or not 
 
-def show_results(computer_score, human_score):
+def show_results(computer_score, human_score):      #function to show the results, who won and by how much when the game has ended
     
