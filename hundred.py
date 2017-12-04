36b14e6c7cc332efd013ab3ceb1eb8e502957169
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2017-12-04 17:07:37 +0000

    defined all the functions

---
+++ hundred.py
@@ -0,0 +1,21 @@
+'''Haroon SHINWARI
+
+
+fill in description of the game'''
+
+def main():
+
+def rules():
+
+def human_move(computer_score, human_score):
+
+def computer_move(computer_score, human_score):
+
+def is_game_over(computer_score, human_score):
+
+def roll():
+
+def ask_yes_or_no(prompt):
+
+def show_results(computer_score, human_score):
+    
