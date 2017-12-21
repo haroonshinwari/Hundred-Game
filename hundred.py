3ff3a46a1cb8335f2115037516de78e348e5bf31
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2017-12-21 15:04:32 +0000

    defined is game over

--- hundred.py
+++ hundred.py
@@ -12,7 +12,7 @@ the other player is given another turn, and whomever scores highest wins, if the
 import random
 
 def main():                                         #this is the fuction that will start the programme for execution
-    rules()
+
 def rules():                                        #function for printing the rules and instructions for the player
     print("This is a simple die game with the objective to score 100 first.\nAt each turn you must choose to roll or not to roll.\nYou may roll as many times as you wish, and stop whenever you like.\nEverytime you roll you increase your cumulative score for that turn by the amount rolled.\nand this is added to your total score at the end of your turn.\nHowever if you roll a 1, your cumulative score for that turn goes to 0 and it is the next person’s turn.\n\nGiven the computer starts first, it has an advantage.\nTherefore you will have an extra turn if the computer reaches 100 first.\nThe winner is then decided by whoever has the highest score.\nIf it is tied then you keep rolling until whoever scores highest.") 
 def human_move(computer_score, human_score):        #function to run when it is the human turn/move
@@ -20,7 +20,12 @@ def human_move(computer_score, human_score):        #function to run when it is 
 def computer_move(computer_score, human_score):     #function to run when it is the computer's turn/move
 
 def is_game_over(computer_score, human_score):      #function to determine whether there is a winner or not and if the game is over
-
+      if (computer_score >= 100 or human_score >= 100) and computer_score != human_score:
+        print('GAME OVER')
+        return True
+    else:
+        print('not game over')
+        return False
 def roll():                                         #function to roll the die randomly        
     return random.randint(1,6)
 def ask_yes_or_no(prompt):                          #function to ask if the user would like to roll again or not 
@@ -28,4 +33,3 @@ def ask_yes_or_no(prompt):                          #function to ask if the user
 def show_results(computer_score, human_score):      #function to show the results, who won and by how much when the game has ended
     
 
-main()
