d07e35119050dc4b637703b9ba223155948334eb
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2017-12-21 14:09:39 +0000

    defined roll importing random function

--- hundred.py
+++ hundred.py
@@ -3,6 +3,8 @@
 
 fill in description of the game'''
 
+import random
+
 def main():                                         #this is the fuction that will start the programme for execution
 
 def rules():                                        #function for printing the rules and instructions for the player
@@ -14,7 +16,7 @@ def computer_move(computer_score, human_score):     #function to run when it is 
 def is_game_over(computer_score, human_score):      #function to determine whether there is a winner or not and if the game is over
 
 def roll():                                         #function to roll the die randomly        
-
+    return random.randint(1,6)
 def ask_yes_or_no(prompt):                          #function to ask if the user would like to roll again or not 
 
 def show_results(computer_score, human_score):      #function to show the results, who won and by how much when the game has ended
